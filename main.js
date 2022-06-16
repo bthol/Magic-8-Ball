2224430d093c76e4430bc5a08d659bcc4ab545f9
bthol <95544562+bthol@users.noreply.github.com>
2022-06-15 21:28:09 -0700

    image assets + new update appraoch

--- main.js
+++ main.js
@@ -1,21 +1,28 @@
-console.log("JS connected");
+const img = document.querySelector(`#eight-ball`);
+img.addEventListener("click", () => {
+    img.src = `${answers[Math.floor(Math.random() * answers.length)]}`;
+});
+
+document.querySelector(`#btn-reset`).addEventListener("click", () => {
+    img.src = "./assets/magic8ball_extra.png";
+});
 
 const answers = [
-    "signs point to yes",
-    "try again later"
+    "./assets/magic8ball_4.png",
+    "./assets/magic8ball_5.png",
+    "./assets/magic8ball_6.png",
+    "./assets/magic8ball_7.png",
+    "./assets/magic8ball_8.png",
+    "./assets/magic8ball_9.png",
+    "./assets/magic8ball_10.png",
+    "./assets/magic8ball_11.png",
+    "./assets/magic8ball_12.png",
+    "./assets/magic8ball_13.png",
+    "./assets/magic8ball_14.png",
+    "./assets/magic8ball_15.png",
+    "./assets/magic8ball_16.png",
+    "./assets/magic8ball_17.png",
+    "./assets/magic8ball_18.png",
+    "./assets/magic8ball_19.png",
+    "./assets/magic8ball_20.png",
 ];
-
-const textEl = document.querySelector(`#text-element`);
-textEl.addEventListener("click", generateAnswer);
-
-const btn1 = document.querySelector(`.button`);
-btn1.addEventListener("click", init);
-
-function generateAnswer() {
-    textEl.innerText = answers[Math.floor(Math.random() * answers.length)];
-};
-
-function init() {
-    textEl.innerText = "click me";
-};
-init();
