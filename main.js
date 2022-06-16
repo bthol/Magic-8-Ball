e46d1c84be72b3b6e6632ef3448833a294ffaa67
bthol <95544562+bthol@users.noreply.github.com>
2022-06-15 20:04:07 -0700

    style change

--- main.js
+++ main.js
@@ -8,6 +8,14 @@ const answers = [
 const textEl = document.querySelector(`#text-element`);
 textEl.addEventListener("click", generateAnswer);
 
+const btn1 = document.querySelector(`.button`);
+btn1.addEventListener("click", init);
+
 function generateAnswer() {
     textEl.innerText = answers[Math.floor(Math.random() * answers.length)];
 };
+
+function init() {
+    textEl.innerText = "click me";
+};
+init();
